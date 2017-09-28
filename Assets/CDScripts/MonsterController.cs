b7f8b00239e76f1faf76b3af2cbad491352da278
Reinhart De Lille <reinhartdelille@hotmail.com>
2017-09-28 21:22:59 +0200

    Initial Commit new repo

---
+++ Assets/CDScripts/MonsterController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterController : MonoBehaviour {
+
+    public float walkSpeed = 1.0f;
+    public float runSpeed = 2.0f;
+
+    Animator anim = null;
+  
+    UnityEngine.AI.NavMeshAgent navAgent = null;
+
+
+	// Use this for initialization
+	void Start () {
+
+        anim= GetComponent<Animator>();
+
+        navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        string EnemyType = "BigMonster";
+
+        if (EnemyType == "BigMonster")
+        {
+
+           
+            anim.SetFloat("Speed", 2.0f);
+            navAgent.speed = runSpeed;
+        }
+
+      
+    }
+
+}
